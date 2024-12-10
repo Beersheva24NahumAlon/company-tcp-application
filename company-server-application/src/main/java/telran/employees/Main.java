6604ed6b1dfa41e695625ea8cbd1fb0f7062ca5b
Nahum Alon <nochum.shepsl@gmail.com>
2024-12-10 22:10:02 +0200

    Added oportunity to gracefull shutdown server

--- company-server-application/src/main/java/telran/employees/Main.java
+++ company-server-application/src/main/java/telran/employees/Main.java
@@ -5,12 +5,14 @@ import telran.net.*;
 
 import static telran.employees.ServerConfigProperties.*;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         Company company = new CompanyImpl();
         Protocol protocol = new ProtocolEmployee(company);
-        TcpServer server = new TcpServer(protocol, PORT);
+        TcpServer server = new TcpServer(protocol, PORT, BAD_RESPONSES, REQUEST_PER_SECOND, TOTAL_TIMEOUT);
         CompanySaver companySaver = new CompanySaver(company);
         if (company instanceof Persistable persistable) {
             persistable.restoreFromFile(FILE_NAME);
@@ -20,6 +22,16 @@ public class Main {
         Thread threadTcpServer = new Thread(server);
         Thread threadCompanySaver = new Thread(companySaver);
         threadTcpServer.start();
+        Scanner scanner = new Scanner(System.in); 
+        while (true) {
+            System.out.print("To shutdown server input \"shutdown\":");
+            String command = scanner.nextLine();
+            if (command.equals("shutdown")) {
+                server.shutdown();
+                break;
+            }
+        }
+        scanner.close();
         threadCompanySaver.setDaemon(true);
         threadCompanySaver.start();
     }
--- company-server-application/src/main/java/telran/employees/ServerConfigProperties.java
+++ company-server-application/src/main/java/telran/employees/ServerConfigProperties.java
@@ -4,4 +4,7 @@ public interface ServerConfigProperties {
     int PORT = 4000;
     String FILE_NAME = "employees.data";
     int SAVING_DELAY = 5;
+    int BAD_RESPONSES = 20;
+    int REQUEST_PER_SECOND = 100;
+    int TOTAL_TIMEOUT = 50000;
 }
